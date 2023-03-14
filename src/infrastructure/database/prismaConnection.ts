26c8b3b81373948b9ea1a156e231863b57987d24
Javier Mesa <fjmh89@icloud.com>
2023-03-14 16:35:07 +0100

    print Prisma logs using Nest

--- src/infrastructure/database/prismaConnection.ts
+++ src/infrastructure/database/prismaConnection.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 export class PrismaConnection {
@@ -5,9 +6,46 @@ export class PrismaConnection {
 
   static getClient() {
     if (!this.client) {
-      this.client = new PrismaClient({
-        log: ['query', 'info', 'warn', 'error'],
+      const prisma = new PrismaClient({
+        log: [
+          {
+            emit: 'event',
+            level: 'query',
+          },
+          {
+            emit: 'event',
+            level: 'error',
+          },
+          {
+            emit: 'event',
+            level: 'info',
+          },
+          {
+            emit: 'event',
+            level: 'warn',
+          },
+        ],
       });
+
+      prisma.$on('query', (e) => {
+        Logger.debug(`Query: ${e.query}`);
+        Logger.debug(`Params: ${e.params}`);
+        Logger.debug(`Duration: ${e.duration} ms`);
+      });
+
+      prisma.$on('error', (e) =>
+        Logger.error(`Message: ${e.message} - Target: ${e.target}`),
+      );
+
+      prisma.$on('info', (e) =>
+        Logger.log(`Message: ${e.message} - Target: ${e.target}`),
+      );
+
+      prisma.$on('warn', (e) =>
+        Logger.warn(`Message: ${e.message} - Target: ${e.target}`),
+      );
+
+      this.client = prisma;
     }
 
     return this.client;
